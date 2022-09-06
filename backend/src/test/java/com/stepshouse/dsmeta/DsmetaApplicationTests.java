2c1ff431d715ea86459edcd20d4b176ae1b9b2f0
Pedro Henrique Passos <56487862+pedropassos6@users.noreply.github.com>
2022-09-06 13:51:49 -0300

    project created

---
+++ backend/src/test/java/com/stepshouse/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepshouse.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
