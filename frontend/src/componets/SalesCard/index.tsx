9635ac34b14ea758693afea6f6d5c8a468214287
Pedro Henrique Passos <56487862+pedropassos6@users.noreply.github.com>
2022-09-11 17:00:54 -0300

    Update index.tsx

--- frontend/src/componets/SalesCard/index.tsx
+++ frontend/src/componets/SalesCard/index.tsx
@@ -13,9 +13,9 @@ function SalesCard() {
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const max = new Date();
 
-    const [minDate, SetMinDate] = useState(min);
+    const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
-    
+
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
@@ -38,7 +38,7 @@ function SalesCard() {
                 <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={minDate}
-                        onChange={(date: Date) => setMinDate(date)}
+                        onChange={(date: Date) => setMinDate (date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
